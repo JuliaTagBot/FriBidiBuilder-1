e605cb99a771879ad9611472392537b4d74caf71
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-08-27 14:43:03 +0200

    Build for all platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,9 +7,8 @@ version = v"1.0.5"
 
 # Collection of sources required to build FriBidi
 sources = [
-    "https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2" =>
+    "https://github.com/fribidi/fribidi/releases/download/v$(version)/fribidi-$(version).tar.bz2" =>
     "6a64f2a687f5c4f203a46fa659f43dd43d1f8b845df8d723107e8a7e6158e4ce",
-
 ]
 
 # Bash recipe for building across all platforms
@@ -23,9 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -35,9 +32,8 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
